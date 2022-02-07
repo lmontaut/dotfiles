fc228604fcb4d60700f4757432ea69d6e9dafb22
Louis Montaut <louismontaut@gmail.com>
2022-02-07 16:09:37 +0100

    Update config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -74,13 +74,8 @@ lvim.plugins = {
             height = 25; -- Height of the floating window
             default_mappings = true; -- Bind default mappings
             debug = false; -- Print debug information
-            opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
+            opacity = 5; -- 0-100 opacity level of the floating window where 100 is fully transparent.
             post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
-            -- You can use "default_mappings = true" setup option
-            -- Or explicitly set keybindings
-            -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
-            -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
-            -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
         }
       end
     },
