fe0f1db9da79682a393e308abe16ab1433bf15c9
lmontaut <louismontaut@gmail.com>
2022-05-15 21:29:45 +0200

    dashboard; more space in nvimtree

--- nvim/.config/nvim/lua/config/plugins_config/dashboard.lua
+++ nvim/.config/nvim/lua/config/plugins_config/dashboard.lua
@@ -1,2 +1,3 @@
 -- Default value is clap
 vim.g.dashboard_default_executive ='telescope'
+vim.cmd("let g:dashboard_custom_shortcut={ 'last_session'       : 'SPC v l', 'find_history'       : 'SPC f r', 'find_file'          : 'SPC f f', 'new_file'           : 'SPC c n', 'change_colorscheme' : 'SPC v p', 'find_word'          : 'SPC f g', 'book_marks'         : 'SPC f R', }")
