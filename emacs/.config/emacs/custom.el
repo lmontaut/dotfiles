1f573a3b2ffdfd43591d26c5fe40b2d783bb98af
Louis Montaut <louismontaut@gmail.com>
2022-05-18 16:14:05 +0200

    custom emacs theme

---
+++ emacs/.config/emacs/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(adwaita)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "black"))))
+ '(isearch ((t (:background "firebrick" :foreground "white"))))
+ '(ivy-current-match ((t (:extend t :background "steel blue" :foreground "white"))))
+ '(ivy-minibuffer-match-face-2 ((t (:background "MistyRose1" :weight bold))))
+ '(lazy-highlight ((t (:background "LightSkyBlue1")))))
