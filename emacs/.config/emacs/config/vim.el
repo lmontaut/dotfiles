07d1bc3b771f7aae8bc49f30cbdf2313bd3425da
Louis Montaut <louismontaut@gmail.com>
2022-05-18 17:31:59 +0200

    emacs vim: visual outside buffers, commentary

--- emacs/.config/emacs/config/vim.el
+++ emacs/.config/emacs/config/vim.el
@@ -8,7 +8,21 @@
   (setq evil-want-C-i-jump nil)
   (setq evil-undo-system 'undo-redo)
   :config
-  (evil-mode 1))
+  (evil-mode 1)
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  ;; Start in normal mode
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal)
+)
+
+(use-package evil-commentary
+  :straight t
+  :config
+  (evil-commentary-mode)
+)
 
 ;; key-chord allows to type sequences of keys
 (use-package key-chord
@@ -17,5 +31,6 @@
   (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
   :config
   (key-chord-mode 1))
+
 
 (provide 'vim)
