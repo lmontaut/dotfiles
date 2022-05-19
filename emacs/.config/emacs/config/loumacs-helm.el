6edf9a69b118944029e979b5f99ed9cb356ee723
Louis Montaut <louismontaut@gmail.com>
2022-05-19 18:48:32 +0200

    emacs: adding helm

--- emacs/.config/emacs/config/loumacs-helm.el
+++ emacs/.config/emacs/config/loumacs-helm.el
@@ -1,15 +1,25 @@
-;; Better help with Helpful & counsel
+;; Helm
+(use-package helm
+  :straight t
+  :bind
+  ([remap find-file] . #'helm-find-files)
+  ([remap execute-extended-command] . #'helm-M-x)
+  ([remap switch-to-buffer] . #'helm-mini)
+)
+(helm-mode)
+
+;; Better help with helpful
 (use-package helpful
   :straight t
-  :after counsel
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key)
+  ;; :after counsel
+  ;; :custom
+  ;; (counsel-describe-function-function #'helpful-callable)
+  ;; (counsel-describe-variable-function #'helpful-variable)
+  ;; :bind
+  ;; ([remap describe-function] . counsel-describe-function)
+  ;; ([remap describe-command] . helpful-command)
+  ;; ([remap describe-variable] . counsel-describe-variable)
+  ;; ([remap describe-key] . helpful-key)
 )
 
 (provide 'loumacs-helm)
