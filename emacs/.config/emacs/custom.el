f1e501395e37f943a59dd082a42443cc884f00d9
Louis Montaut <louismontaut@gmail.com>
2022-05-19 15:28:54 +0200

    emacs theme: lsp colors

--- emacs/.config/emacs/custom.el
+++ emacs/.config/emacs/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(conda-anaconda-home "/home/lou/anaconda3/")
  '(custom-enabled-themes '(adwaita)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -14,4 +15,7 @@
  '(isearch ((t (:background "firebrick" :foreground "white"))))
  '(ivy-current-match ((t (:extend t :background "steel blue" :foreground "white"))))
  '(ivy-minibuffer-match-face-2 ((t (:background "MistyRose1" :weight bold))))
- '(lazy-highlight ((t (:background "LightSkyBlue1")))))
+ '(lazy-highlight ((t (:background "LightSkyBlue1"))))
+ '(line-number-current-line ((t :weight bold :foreground "snow" :background "deep pink")))
+ '(lsp-face-highlight-read ((t (:background "hot pink" :foreground "snow"))))
+ '(lsp-face-highlight-write ((t (:background "hot pink" :foreground "snow" :weight bold)))))
