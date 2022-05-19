3441f91d7497fe1870c8b989ea334beab54807e0
Louis Montaut <louismontaut@gmail.com>
2022-05-19 10:13:34 +0200

    emacs: spacemacs-theme loaded but not used - allows to swap to dark spacemacs if needed

--- emacs/.config/emacs/config/theme.el
+++ emacs/.config/emacs/config/theme.el
@@ -1,9 +1,11 @@
 ; Custom theme based on adwaita is in custom.el
-;(use-package spacemacs-theme
-;  :straight t
-;  :defer t
-;  :init
-;  (load-theme 'spacemacs-light t))
+(use-package spacemacs-theme
+  :straight t
+  :defer t
+  :init
+;  (load-theme 'spacemacs-light t)
+;  (load-theme 'spacemacs-dark t)
+); have it available if needed
 
 (use-package doom-modeline
   :straight t
