a26350b49e45d7a66cf2fd967f29170bc09ead32
Louis Montaut <louismontaut@gmail.com>
2022-05-20 18:36:27 +0200

    emacs: custom theme changes

--- emacs/.config/emacs/custom.el
+++ emacs/.config/emacs/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(conda-anaconda-home "/home/lou/anaconda3/")
- '(custom-enabled-themes '(adwaita)))
+ '(custom-enabled-themes '(adwaita))
+ '(helm-minibuffer-history-key "M-p"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -12,10 +13,12 @@
  ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "black"))))
  '(font-lock-comment-face ((t (:background "misty rose" :foreground "sienna"))))
+ '(git-gutter:added ((t (:inherit default :foreground "dark green" :weight bold))))
+ '(highlight ((t (:background "pale turquoise" :foreground "black"))))
  '(isearch ((t (:background "firebrick" :foreground "white"))))
  '(ivy-current-match ((t (:extend t :background "steel blue" :foreground "white"))))
  '(ivy-minibuffer-match-face-2 ((t (:background "MistyRose1" :weight bold))))
  '(lazy-highlight ((t (:background "LightSkyBlue1"))))
  '(line-number-current-line ((t :weight bold :foreground "snow" :background "deep pink")))
- '(lsp-face-highlight-read ((t (:background "hot pink" :foreground "snow"))))
- '(lsp-face-highlight-write ((t (:background "hot pink" :foreground "snow" :weight bold)))))
+ '(lsp-face-highlight-read ((t (:inherit highlight))))
+ '(lsp-face-highlight-write ((t (:inherit highlight)))))
