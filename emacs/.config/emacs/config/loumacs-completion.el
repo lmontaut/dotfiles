dde9b336227861fea4cc6bad3523178bdd79a9fc
Louis Montaut <louismontaut@gmail.com>
2022-05-20 19:29:24 +0200

    emacs: tab to select completion

--- emacs/.config/emacs/config/loumacs-completion.el
+++ emacs/.config/emacs/config/loumacs-completion.el
@@ -3,6 +3,14 @@
 (use-package company
   :straight t
   :config
-  (add-hook 'after-init-hook 'global-company-mode))
+  (add-hook 'after-init-hook 'global-company-mode)
+  (setq company-minimum-prefix-length 1
+        company-idle-delay 0.0)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+)
+(global-company-mode)
 
 (provide 'loumacs-completion)
