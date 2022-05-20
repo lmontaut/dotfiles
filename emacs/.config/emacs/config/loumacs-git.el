7ce7144262a9514082947e1fca6e6c509152c5ba
Louis Montaut <louismontaut@gmail.com>
2022-05-20 18:34:39 +0200

    emacs: added magit

---
+++ emacs/.config/emacs/config/loumacs-git.el
@@ -0,0 +1,20 @@
+;; show hunk on the sidebar
+(use-package git-gutter
+  :straight t
+  :config
+  (add-hook 'prog-mode-hook 'git-gutter-mode)
+)
+
+;; Magit
+(use-package magit
+  :straight t
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package evil-magit
+  :straight t
+  :after magit)
+
+
+(provide 'loumacs-git)
