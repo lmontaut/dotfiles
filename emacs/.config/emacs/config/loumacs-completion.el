910ead7e86b9ea870e73968c18b78baffdcac77d
lmontaut <louismontaut@gmail.com>
2022-05-22 18:38:28 +0200

    emacs: autocomplete delay

--- emacs/.config/emacs/config/loumacs-completion.el
+++ emacs/.config/emacs/config/loumacs-completion.el
@@ -5,7 +5,7 @@
   :config
   (add-hook 'after-init-hook 'global-company-mode)
   (setq company-minimum-prefix-length 1
-        company-idle-delay 0.0)
+        company-idle-delay 0.2)
   :bind (:map company-active-map
          ("<tab>" . company-complete-selection))
         (:map lsp-mode-map
