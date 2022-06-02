49c1ae62f8d71255c9d95b0cc7ccd8ce6dccf735
Louis Montaut <louismontaut@gmail.com>
2022-06-02 09:54:31 +0200

    toggleterm toggleterming

--- nvim/.config/nvim/lua/config/plugins_config/toggleterm.lua
+++ nvim/.config/nvim/lua/config/plugins_config/toggleterm.lua
@@ -30,10 +30,10 @@ function _G.set_terminal_keymaps()
   -- vim.api.nvim_buf_set_keymap(0 , 't' , '<esc>' , [[<C-\><C-n>]]               , opts)
   vim.api.nvim_buf_set_keymap(0 , 't' , '<C-t>' , [[<C-\><C-n>]] , opts)
   -- vim.api.nvim_buf_set_keymap(0 , 't' , 'jk'    , [[<C-\><C-n>]]               , opts)
-  -- vim.api.nvim_buf_set_keymap(0 , 't' , '<C-h>' , [[<C-\><C-n><C-W>h]]         , opts)
-  -- vim.api.nvim_buf_set_keymap(0 , 't' , '<C-j>' , [[<C-\><C-n><C-W>j]]         , opts)
-  -- vim.api.nvim_buf_set_keymap(0 , 't' , '<C-k>' , [[<C-\><C-n><C-W>k]]         , opts)
-  -- vim.api.nvim_buf_set_keymap(0 , 't' , '<C-l>' , [[<C-\><C-n><C-W>l]]         , opts)
+  vim.api.nvim_buf_set_keymap(0 , 't' , '<C-h>' , [[<C-\><C-n><C-W>h]]         , opts)
+  vim.api.nvim_buf_set_keymap(0 , 't' , '<C-j>' , [[<C-\><C-n><C-W>j]]         , opts)
+  vim.api.nvim_buf_set_keymap(0 , 't' , '<C-k>' , [[<C-\><C-n><C-W>k]]         , opts)
+  vim.api.nvim_buf_set_keymap(0 , 't' , '<C-l>' , [[<C-\><C-n><C-W>l]]         , opts)
 end
 
 -- local opts = { noremap = true, silent = true }
