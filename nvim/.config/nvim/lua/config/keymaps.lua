f13e53444fa5862003e2da4b31fc13c9d160e2a5
Louis Montaut <louismontaut@gmail.com>
2022-07-24 10:15:31 +0200

    Update filetype q-quit

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -80,6 +80,7 @@ autocmd FileType qf nnoremap <buffer> q :q<CR>
 autocmd FileType git nnoremap <buffer> q :q<CR>
 autocmd FileType vim nnoremap <buffer> q :q<CR>
 autocmd FileType vimcmake nnoremap <buffer> q :q<CR>
+autocmd FileType lspinfo nnoremap <buffer> q :q<CR>
 ]]
 
 -- Visual Block -> Does not work
