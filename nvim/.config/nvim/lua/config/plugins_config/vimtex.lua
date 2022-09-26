a1e547f0475b2a1a8c7595e1576490adf5106d20
Louis Montaut <louismontaut@gmail.com>
2022-09-26 16:26:51 +0200

    Update vimtex for macos

--- nvim/.config/nvim/lua/config/plugins_config/vimtex.lua
+++ nvim/.config/nvim/lua/config/plugins_config/vimtex.lua
@@ -1,4 +1,5 @@
-vim.g.vimtex_view_method = 'zathura'
+vim.g.tex_flavor = "latex"
+vim.g.vimtex_view_method = "skim"
 vim.g.vimtex_compiler_latexmk = { build_dir = "build" }
 vim.g.vimtex_enabled = 1
 vim.g.vimtex_quickfix_mode = 0
